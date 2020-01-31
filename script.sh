5e6dcc5edbedffbdf080a3616c2f66955cd6e414
bdonnelly12 <bdonnelly12@my.bcit.ca>
2020-01-31 12:08:13 -0800

    Testing different packages

--- script.sh
+++ script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo "Checking for packages"
 sudo apt update && sudo apt upgrade
-sudo apt install genisoimage
+sudo apt install xorriso
+sudo apt install isohybrid
+sudo apt install isolinux
 sudo apt install bsdtar
 mkdir isofiles
 bsdtar -xf debian-10.2.0-amd64-xfce-CD-1.iso -C isofiles
@@ -15,7 +17,10 @@ chmod +w md5sum.txt
 md5sum `find -follow -type f` > md5sum.txt
 chmod -w md5sum.txt
 cd ..
-sudo genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-debian-10.2.0-i386-netinst.iso isofiles
+xorriso -as mkisofs -o preseed-debian-10.2.0-i386-netinst.iso \
+	        -isohybrid-mbr /usr/lib/ISOLINUX/isohdpfx.bin \
+		        -c isolinux/boot.cat -b isolinux/isolinux.bin -no-emul-boot \
+			        -boot-load-size 4 -boot-info-table isofiles
 sudo rm -r isofiles
 echo "Preseed Image Created"
 
