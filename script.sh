fe118ccc71b104351e031abdf5b6f88a135721c3
bdonnelly12 <bdonnelly12@my.bcit.ca>
2020-01-31 11:05:01 -0800

    Fixed type

--- script.sh
+++ script.sh
@@ -15,7 +15,7 @@ chmod +w md5sum.txt
 md5sum `find -follow -type f` > md5sum.txt
 chmod -w md5sum.txt
 cd ..
-sudo enisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-debian-10.2.0-i386-netinst.iso isofiles
+sudo genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-debian-10.2.0-i386-netinst.iso isofiles
 sudo rm -r isofiles
 echo "Preseed Image Created"
 
