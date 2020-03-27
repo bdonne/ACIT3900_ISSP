410d71da2c52763b21296e75a9b0b7022dc2a431
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-03-27 00:36:44 -0700

    Delete chromeKiosk.sh

--- Kiosk_Config/chromeKiosk.sh
+++
@@ -1,4 +0,0 @@
-#!bin/bash
-Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
-Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-chromium --kiosk --start-maximized --window-size=$Xaxis,$Yaxis --window-position=0,0
