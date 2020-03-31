9953bbf73daa070017f4c0bbbbd39597bb45ed30
JIAJUNATBCIT <49431626+JIAJUNATBCIT@users.noreply.github.com>
2020-03-31 13:29:48 -0700

    Update firefoxKiosk.sh

--- Kiosk_conf_2/firefoxKiosk.sh
+++ Kiosk_conf_2/firefoxKiosk.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
 Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
-su student
+su vagrant
 firefox http://learn.bcit.ca --kiosk --start-maximized --window-size=9000,9000 --window-position=0,0
